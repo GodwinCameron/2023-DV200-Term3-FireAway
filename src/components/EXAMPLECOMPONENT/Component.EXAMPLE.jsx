cba651173f4366aa59fcb34cc919d0c3478036cf
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-07-10 10:57:47 +0300

    Setup test component and installed Axios, React-router-dom and SASS packages
    Started cleaning up the React boiler plate, added some packages and setup basic example component and linking

---
+++ src/components/EXAMPLECOMPONENT/Component.EXAMPLE.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Style.EXAMPLE.module.scss";
+
+const EXAMPLE = () => {
+    return(<>
+        <div className={style.test}></div>
+    </>)
+}
+
+export default EXAMPLE;
