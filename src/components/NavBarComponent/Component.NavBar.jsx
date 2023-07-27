0498310285ba803314fcfb8d39990bf2c71c6091
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-07-27 18:17:11 +0300

    NavBar setup + profile and cart icons added
    Started the Navbar

--- src/components/NavBarComponent/Component.NavBar.jsx
+++ src/components/NavBarComponent/Component.NavBar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import style from "./Style.NavBar.module.scss";
+import cartIcon from "../../assets/icons/cart.svg";
+import profileIcon from "../../assets/icons/profile.svg";
 
 const NavBarComponent = () => {
     return(<>
-        <div className={style.main}></div>
+        <div className={style.main}>
+            <div className={style.topSection}>
+                <div className={style.logo}></div>
+                <div className={style.searchBarArea}><input className={style.searchBar} placeholder="Search Keyword or Phrase"></input></div>
+                <div className={style.searchBarWidgets}>
+                    <div className={style.navCartIcon}><img src={cartIcon}/></div><div id="nav_cart_items" className={style.cartItems}>23</div>
+                    <div className={style.navProfileIcon}><img src={profileIcon}/></div>
+                </div>
+            </div>
+        </div>
     </>)
 }
 
