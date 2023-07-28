cf3d1fc450e5ce34cea51b7aea51c367551f2968
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-07-28 16:30:25 +0300

    Image Slider Setup
    setting up image slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import EXAMPLE from './components/EXAMPLECOMPONENT/Component.EXAMPLE';
 import NavBarComponent from './components/NavBarComponent/Component.NavBar';
+import ImageSlider from './components/ImageSliderComponent/Component.ImageSlider';
 
 function App() {
-  return (
+  return (<>
     <NavBarComponent />
-  );
+    <ImageSlider />
+    </>);
 }
 
 export default App;
---
+++ src/components/ImageSliderComponent/Component.ImageSlider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./Style.ImageSlider.module.scss";
+
+const ImageSlider = () => {
+  return (
+    <>
+      <div className={style.main}>
+        <div className={style.dot}></div>
+        <div className={style.dot}></div>
+        
+      </div>
+    </>
+  );
+};
+
+export default ImageSlider;
