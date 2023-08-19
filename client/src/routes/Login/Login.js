a73d5ab568f61d686e559581ef464f688980efbc
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-08-20 00:37:30 +0200

    Tweaked Login error handling for missing information and navigation
    added error handling for missing username and password, and added navigation to login

--- client/src/routes/Login/Login.js
+++ client/src/routes/Login/Login.js
@@ -1,73 +1,82 @@
 import React from "react";
 import style from "./Style.Login-Route.module.scss";
 import FooterComponent from "../../components/FooterComponent/Component.Footer";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useState } from "react";
 
 const LoginRoute = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const registered = queryParams.get("registered");
+  const [divStyle, setDivStyle] = useState({});
 
-    // const loginUser = async (e) => {
-  //   try {
-  //     const response = await axios.post("/api/auth", userInfo);
-  //     const { token } = response.data;
+  const badLogin = () => {
+    const styling = {
+      backgroundColor: "rgba(255, 0, 0, 0.2)",
+      border: "1px solid red",
+      color: "red",
+    };
+    setDivStyle(styling);
+  };
 
-  //     localStorage.setItem("JWT", token);
-  //   } catch (error) {
-  //     console.error("Authentication Error: ", error);
-  //   }
-  // };
+  const navigate = useNavigate();
 
-  // const testUser = () => {
-  //   const ActiveJWT = localStorage.getItem("JWT");
-  //   console.log("tested user with token: ", ActiveJWT);
-  // };
-
-  // const loginHandler = (event) => {
-  //   event.preventDefault();
-  // call server to login
-  //   try {
-  //     const response = axios.post("/api/login", {
-  //       username: document.getElementById("username").value,
-  //       password: document.getElementById("password").value,
-  //     });
-  //     const { token } = response.data;
-  //     localStorage.setItem("JWT", token);
-  //   } catch (error) {
-  //     console.error("Authentication Error: ", error);
-  //   }
-  //   console.log(email);
-  // };
+  const ToHome = () => {
+    navigate("/");
+  };
+  const ToRegister = () => {
+    navigate("/register");
+  };
 
   const loginHandler = (event) => {
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const userInfo = { username, password };
-    console.log(userInfo);
 
-    axios.post("http://localhost:5000/api/login", userInfo)
-    .then(res => {
-      console.log(res);
-      localStorage.setItem("JWT", res.data.token);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+    if (document.getElementById("username").value === "") {
+      document.getElementById("messageText").innerHTML =
+        "Please enter a valid username";
+      badLogin();
+      return;
+    } else if (document.getElementById("password").value === "") {
+      document.getElementById("messageText").innerHTML =
+        "Please enter a password";
+      badLogin();
+      return;
+    } else {
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const userInfo = { username, password };
+      console.log(userInfo);
 
+      axios
+        .post("http://localhost:5000/api/login", userInfo)
+        .then((res) => {
+          console.log(res);
+          localStorage.setItem("JWT", res.data.token);
+          ToHome();
+        })
+        .catch((err) => {
+          console.log(err);
+          document.getElementById("messageText").innerHTML =
+            "Invalid username or password";
+          badLogin();
+        });
+    }
+  };
 
   return (
     <>
       <div className={style.main}>
         <h1>Login</h1>
         {registered === "true" ? (
-          <div className={style.message}>
-            <p>Registration Complete! Please login</p>
+          <div style={divStyle} className={style.message}>
+            <p id="messageText">Registration Complete! Please login</p>
           </div>
-        ) : null}
+        ) : (
+          <div style={divStyle} className={style.message2}>
+            <p id="messageText"></p>
+          </div>
+        )}
         <form className={style.loginForm} onSubmit={loginHandler}>
           <input
             id="username"
@@ -82,6 +91,9 @@ const LoginRoute = () => {
           ></input>
           <button className={style.button}>Login</button>
         </form>
+          <p id="goRegister" className={style.noAcc} onClick={ToRegister}>Don't have an account? <span className={style.reg}>Register here!</span></p>
+          <p>Or</p>
+          <div onClick={ToHome} className={style.noLogin}>Return home without Login</div>
       </div>
       <FooterComponent />
     </>
